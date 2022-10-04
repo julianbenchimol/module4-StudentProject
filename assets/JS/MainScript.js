111043892695fc067b43a4533656a67b0cb8d58c
Julian Benchimol <jbenchimol1999@gmail.com>
2022-10-04 13:36:44 -0400

    Got letter check working

--- assets/JS/MainScript.js
+++ assets/JS/MainScript.js
@@ -5,7 +5,9 @@
 //TODO: Create a timer for 20s that updates in real time
 //!Done
 //TODO: Get key down to check what key is pressed
+//!Done
 //TODO: Assign the key to a variabe and check if the variable is contained within the chosen word
+//!DONE
 //TODO: If the pressed key is in, reveal it, otherwise take a life
 //TODO: Update word-view, timer-element, wins & losses to reflect game choices
 //TODO: Display a win/loss message when the word is guessed or the timer runs out/runs out of lives
@@ -24,44 +26,52 @@ var wins = 0;
 var losses = 0;
 var lives = 5; 
 var timeLeft = 20;
+var guessedLetters = Array();
+var playerWord = Array();
+
 
 //HTML Elements
-var startButton = document.querySelector("#start-game")
+var startButton = document.querySelector("#start-game");
 var timeElement = document.querySelector("#timer");
-var keyPressed = addEventListener("keydown", GetKey);
+var wordElement = document.querySelector("#word-view");
+var keyElement = document.querySelector("#key");
 
 startButton.addEventListener("click", InitializeGame);
 
+//Gets key Input and displays it on the page
+document.addEventListener("keydown", KeyDownAction);
+
 //Initializes the game
+//!WORKING
 function InitializeGame(){
     if(!gameStarted){
     //Logs when button is pressed 
     //!(working)
     console.log('Game initializing');
     gameStarted = true;
+
     //gets a random number and assigns 'word' to a random word in 'possible words' array
+    //!Working
     var randomNumber = Math.floor(Math.random() * possibleWords.length);
     word = possibleWords[randomNumber];
-    //logs the random word chosen 
-    //!(working)
     console.log(word);
 
     //Turns the chosen word to an array and logs it 
-    //!(working)
-    wordArray = Array.from(word);
-    console.log(wordArray);
+    //!Working
+    wordArray = Array.from(word.toLowerCase());
 
     //Adds a blank space to 'blank array' for each letter in 'word array' and logs it 
-    //!(working)
+    //!Working
     for(var i = 0; i < wordArray.length; i++){
         wordBlankArray.push("_");
     }
-    console.log(wordBlankArray);
 
     //Joins the array using blank spaces, rather than commas, and logs it
     //!Working
-    wordBlankArray = wordBlankArray.join(" ");
-    console.log(wordBlankArray);
+    wordBlank = wordBlankArray.join(" ");
+    console.log(wordBlank);
+
+    wordElement.textContent = wordBlank;
 
     SetTime();
     }
@@ -70,21 +80,53 @@ function InitializeGame(){
     }
     
 }
+
 //Sets the timer and adds lose condition
+//!WORKING
 function SetTime(){
     //creates an interval
+    //!Working
     var timerInterval = setInterval(function(){
         //updates timeLeft and updates the page
+        //!Working
         timeLeft--;
         timeElement.textContent = timeLeft + "s";
 
         //checks for if time = 0 and ends the game
+        //!Working
         if(timeLeft === 0){
             clearInterval(timerInterval);
             EndGame();
         }
     },1000);
 }
-function GetKey(){
-    console.log(keyPressed);
+
+//reads keydown and checks if the key pressed is in the word
+//!WORKING
+function KeyDownAction(event){
+
+    //Stops players from guessing when the game hasn't started
+    if(gameStarted){
+
+        //gets the key and key code from the event
+        var key = event.key.toLowerCase();
+
+        //updates the key element with the pressed key 
+        keyElement.textContent = key;
+
+        //Checks the array if the key pressed is present
+        for (var i = 0; i < wordArray.length; i ++){
+            //if the key is present, then it will add it to an array
+            if(wordArray[i] === key){
+                console.log("letter detected at " + i + " index.");
+                wordBlankArray[i] = key;
+            }
+        }
+        wordBlank = wordBlankArray.join(" ");
+        wordElement.textContent = wordBlank;
+    }
+    //when the player tries to press a key before the game started
+    else {
+        alert("Game Hasn't Started!");
+    }
 }
