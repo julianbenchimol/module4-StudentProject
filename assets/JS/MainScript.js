b64a953261d1844a8d3c39fae6e0912005b5b0f0
Julian Benchimol <jbenchimol1999@gmail.com>
2022-10-04 11:02:31 -0400

    Initilize the game through button (words only)

--- assets/JS/MainScript.js
+++ assets/JS/MainScript.js
@@ -1,5 +1,7 @@
 //TODO: Start Game runs InitializeGame()
+//!Done
 //TODO: Initialize game renders the blanked out word
+//!Done
 //TODO: Get key down to check what key is pressed
 //TODO: Assign the key to a variabe and check if the variable is contained within the chosen word
 //TODO: If the pressed key is in, reveal it, otherwise take a life
@@ -8,33 +10,51 @@
 //TODO: Display a win/loss message when the word is guessed or the timer runs out/runs out of lives
 //TODO: Track wins and losses in local storage
 
-var possibleWords = ["JavaScript", "CSS", "HTML"];
+//Initialize Variables
+var possibleWords = ["JavaScript", "CSS", "HTML", "GitHub"];
+var word = "";
+var wordArray = [];
 
-var chosenWord = "";
+var wordBlank = "";
+var wordBlankArray = new Array();
 
-var blankedWord = [];
-var chosenWordArray = [];
+//Gameplay Variables
+var wins = 0;
+var losses = 0;
+var lives = 5; 
+var timeLeft;
 
+//HTML Elements
+var startButton = document.querySelector("#start-game")
 
-InitializeGame();
+startButton.addEventListener("click", InitializeGame);
 
-//initializes the game
 function InitializeGame(){
-    ChooseWord();
-    SetBlanks();
-}
+    //Logs when button is pressed 
+    //!(working)
+    console.log('Game initializing');
 
-//chooses a random word based on a random number 
-function ChooseWord(){
-    var randomNumber = Math.floor(Math.random(possibleWords.length));
-    chosenWord = possibleWords[randomNumber];
-    console.log(chosenWord);
-}
+    //gets a random number and assigns 'word' to a random word in 'possible words' array
+    var randomNumber = Math.floor(Math.random() * possibleWords.length);
+    word = possibleWords[randomNumber];
+    //logs the random word chosen 
+    //!(working)
+    console.log(word);
 
-//sets a blanked out version of the word in the array
-function SetBlanks(){
-    chosenWordArray = Array.from(chosenWord);
-    for(var i = 0; i < chosenWordArray.length; i++){
-        blankedWord.push("_");
+    //Turns the chosen word to an array and logs it 
+    //!(working)
+    wordArray = Array.from(word);
+    console.log(wordArray);
+
+    //Adds a blank space to 'blank array' for each letter in 'word array' and logs it 
+    //!(working)
+    for(var i = 0; i < wordArray.length; i++){
+        wordBlankArray.push("_");
     }
+    console.log(wordBlankArray);
+
+    //Joins the array using blank spaces, rather than commas, and logs it
+    //!Working
+    wordBlankArray = wordBlankArray.join(" ");
+    console.log(wordBlankArray);
 }
