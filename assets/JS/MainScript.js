81b573338662be74678e984d57a6f2e55a4aec05
Julian Benchimol <jbenchimol1999@gmail.com>
2022-10-04 11:34:49 -0400

    Timer element added and printing to page

--- assets/JS/MainScript.js
+++ assets/JS/MainScript.js
@@ -2,11 +2,12 @@
 //!Done
 //TODO: Initialize game renders the blanked out word
 //!Done
+//TODO: Create a timer for 20s that updates in real time
+//!Done
 //TODO: Get key down to check what key is pressed
 //TODO: Assign the key to a variabe and check if the variable is contained within the chosen word
 //TODO: If the pressed key is in, reveal it, otherwise take a life
 //TODO: Update word-view, timer-element, wins & losses to reflect game choices
-//TODO: Create a timer for 20s that updates in real time
 //TODO: Display a win/loss message when the word is guessed or the timer runs out/runs out of lives
 //TODO: Track wins and losses in local storage
 
@@ -14,26 +15,30 @@
 var possibleWords = ["JavaScript", "CSS", "HTML", "GitHub"];
 var word = "";
 var wordArray = [];
-
 var wordBlank = "";
 var wordBlankArray = new Array();
+var gameStarted = false;
 
 //Gameplay Variables
 var wins = 0;
 var losses = 0;
 var lives = 5; 
-var timeLeft;
+var timeLeft = 20;
 
 //HTML Elements
 var startButton = document.querySelector("#start-game")
+var timeElement = document.querySelector("#timer");
+var keyPressed = addEventListener("keydown", GetKey);
 
 startButton.addEventListener("click", InitializeGame);
 
+//Initializes the game
 function InitializeGame(){
+    if(!gameStarted){
     //Logs when button is pressed 
     //!(working)
     console.log('Game initializing');
-
+    gameStarted = true;
     //gets a random number and assigns 'word' to a random word in 'possible words' array
     var randomNumber = Math.floor(Math.random() * possibleWords.length);
     word = possibleWords[randomNumber];
@@ -57,4 +62,29 @@ function InitializeGame(){
     //!Working
     wordBlankArray = wordBlankArray.join(" ");
     console.log(wordBlankArray);
+
+    SetTime();
+    }
+    else {
+        alert("Game has already started!");
+    }
+    
+}
+//Sets the timer and adds lose condition
+function SetTime(){
+    //creates an interval
+    var timerInterval = setInterval(function(){
+        //updates timeLeft and updates the page
+        timeLeft--;
+        timeElement.textContent = timeLeft + "s";
+
+        //checks for if time = 0 and ends the game
+        if(timeLeft === 0){
+            clearInterval(timerInterval);
+            EndGame();
+        }
+    },1000);
+}
+function GetKey(){
+    console.log(keyPressed);
 }
