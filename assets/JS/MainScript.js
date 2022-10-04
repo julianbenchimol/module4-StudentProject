26d6be286002d7d3d2f387178c7d9e30187cfc36
JulianBenchimol <jbenchimol1999@gmail.com>
2022-10-03 21:47:04 -0400

    Added todo messages

--- assets/JS/MainScript.js
+++ assets/JS/MainScript.js
@@ -1,3 +1,13 @@
+//TODO: Start Game runs InitializeGame()
+//TODO: Initialize game renders the blanked out word
+//TODO: Get key down to check what key is pressed
+//TODO: Assign the key to a variabe and check if the variable is contained within the chosen word
+//TODO: If the pressed key is in, reveal it, otherwise take a life
+//TODO: Update word-view, timer-element, wins & losses to reflect game choices
+//TODO: Create a timer for 20s that updates in real time
+//TODO: Display a win/loss message when the word is guessed or the timer runs out/runs out of lives
+//TODO: Track wins and losses in local storage
+
 var possibleWords = ["JavaScript", "CSS", "HTML"];
 
 var chosenWord = "";
